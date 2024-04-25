8c2682061d95e30f2300f03815355a60afd8108d
Alex Steele <ateele193@cedarville.edu>
2024-04-24 20:01:26 -0400

    Login partial backend

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,35 +1,44 @@
 import { useState } from 'react'
+import axios from 'axios'
 
 import {Helmet} from "react-helmet";
 function Login() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
 
-  return (
-    <>
-    <Helmet>
-           <link href="/Users/alexbluesteele/Documents/WebappsProject6/WebAppsProject6/client/src/Login.css" rel="stylesheet" type="text/css" />
-    </Helmet>
-    <div class="container">
-        <div class="login-container-wrapper">
-            <div class="logo">
+    function handleSubmit(event){
+        event.preventDefault();
+        axios.post('http://localhost:8081/login', {username, password})
+        .then(res => console.log(res))
+        .catch(err=> console.log(err));
+    }
+    return (
+        <>
+        <Helmet>
+            <link href="/Users/alexbluesteele/Documents/WebappsProject6/WebAppsProject6/client/src/Login.css" rel="stylesheet" type="text/css" />
+        </Helmet>
+        <div class="container">
+            <div class="login-container-wrapper">
+                <div class="logo">
+                </div>
+                <div class="welcome"><strong>Welcome,</strong> please login</div>
+
+                <form class="login-form" onSubmit={handleSubmit}>
+                    <div class="form-group">
+                        <input id="login_username" class="form-control" type="username" placeholder="Username" name="username" required onChange={e => setUsername(e.target.value)}/>
+                    </div>
+                    <div class="form-group">
+                        <input id="login_password" class="form-control" type="password" placeholder="Password" name="password" required onChange={e => setPassword(e.target.value)}/>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn">Login</button>
+                    </div>
+                </form>
             </div>
-            <div class="welcome"><strong>Welcome,</strong> please login</div>
-
-            <form class="login-form" method="post" action="php/login.php">
-                <div class="form-group">
-                    <input id="login_username" class="form-control" type="username" placeholder="Username" name="username" required></input>
-                </div>
-                <div class="form-group">
-                    <input id="login_password" class="form-control" type="password" placeholder="Password" name="password" required></input>
-                </div>
-                <div class="form-group">
-                    <button type="submit" class="btn">Login</button>
-                </div>
-            </form>
         </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Login
--- server/server.js
+++ server/server.js
@@ -1,14 +1,36 @@
 const express = require('express');
+const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
-const port = 5000;
-
+const port = 8081;
+app.use(express.json());
 app.use(cors());
 
-app.get("/api", (req, res) => {
-    console.log("Hitting api");
-    res.json({"users": ["userOne", "userTwo", "userThree"]});
-});
+const db = mysql.createConnection({
+    host: "james.cedarville.edu",
+    user: "cs3220_sp24",
+    password: "OqagokbAg9DzKZGb",
+    database: "cs3220_sp24"
+})
 
-app.listen(port, () => {console.log("Server started on port ", port);});
+app.post('/login', (req, res)=> {
+    const sql = "SELECT username, password FROM JAC_users WHERE username= ?;";
+
+    db.query(sql, [req.body.username, req.body.password], (err, data) =>{
+        if(err) {
+            console.error("Error:", err);
+            return res.json("Login Failed");
+        }
+        if(data.length > 0) {
+            //res.redirect("/");
+            return res.json("Login Successful"); // Assuming if data exists, it's a successful login
+        } else {
+            return res.json("Login Failed: Invalid username or password");
+        }
+        
+    });
+})
+app.listen(port, () => {
+    console.log("Listening...");
+});
